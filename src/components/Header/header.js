af32a4d1dd4a9e7c914c611b30245444071c421f
manojpudasaini <mpudasaini17@gmail.com>
2020-07-24 21:22:06 +0545

    sidebar added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
+import Sidebar from './sidebar'
+import MenuIcon from '../../assets/menu-icon.png';
 
 const Header=()=>{
+    const [showSidebar,setShowSidebar]=useState(false);
+   const onSidebarHandler=()=>{
+       if(!showSidebar){
+           setShowSidebar(true);
+       }
+       else{
+            setShowSidebar(false);
+       }
+
+    }
     return(
         <React.Fragment>
                 <div className="container">
@@ -10,7 +22,18 @@ const Header=()=>{
                         <li><a href="">Movies</a></li>
                         <li><a href="">TV Series</a></li>
                     </ul>
+                    <div className="menu-icon">
+                        <img src={MenuIcon} alt="menuicon" onClick={onSidebarHandler}/>
+                    </div>
                 </div>
+                {showSidebar ? <Sidebar/>:null
+
+                                             
+                
+                
+                
+                }
+                
         </React.Fragment>
     )
 }
---
+++ src/components/Header/sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './header.css'
+
+const Sidebar=()=>{
+    return(
+        <React.Fragment>
+            <div className='side-wrapper'>
+                <div>
+
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+export  default Sidebar
