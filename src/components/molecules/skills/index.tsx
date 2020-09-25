a91f35d0ddb56d8638a208ea8bf1d2491214be00
Bikash888 <bikashdulal150@gmail.com>
2020-09-25 09:13:10 +0545

    skill responsiveness completed

--- src/components/molecules/skills/index.tsx
+++ src/components/molecules/skills/index.tsx
@@ -1,93 +1,93 @@
 import React from 'react';
-import {createUseStyles} from 'react-jss';
-import {Row,Col} from "antd";
+import { createUseStyles } from 'react-jss';
+import { Row, Col } from "antd";
 import "antd/dist/antd.css";
-import {FundProjectionScreenOutlined,AntDesignOutlined,ToolOutlined} from "@ant-design/icons";
+import { FundProjectionScreenOutlined, AntDesignOutlined, ToolOutlined } from "@ant-design/icons";
 
-const useStyle= createUseStyles({
-    skillsWrapper:{
-        margin:'20px 0px',
-        height:500,
-        fontFamily:`'Montserrat',sans-serif`,
+const useStyle = createUseStyles({
+    skillsWrapper: {
+        margin: '20px 0px',
+
+        fontFamily: `'Montserrat',sans-serif`,
     },
-    skillsHeader:{
-        margin:'50px 0px',
-        textAlign:'center',
-        fontSize:40,
-        fontWeight:600,
-        color:'#2c5f2d',
+    skillsHeader: {
+        margin: '50px 0px',
+        textAlign: 'center',
+        fontSize: 40,
+        fontWeight: 600,
+        color: '#2c5f2d',
     },
-    mainRow:{
-        backgroundColor:'#101820ff',
-        height:'100%',
-        color:'#fff',
+    mainRow: {
+        backgroundColor: '#101820ff',
+        height: '100%',
+        color: '#fff',
     },
-    
-    skillTitle:{
-        fontSize:30,
-        lineHeight:'64px',
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'center',
-        margin:'10px 0px',
-        '& p':{
-          borderBottom:'1px solid #fff',
-          fontWeight:500,
+
+    skillTitle: {
+        fontSize: 30,
+        lineHeight: '64px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '10px 0px',
+        '& p': {
+            borderBottom: '1px solid #fff',
+            fontWeight: 500,
         },
-       
-        '& li':{
-          listStyle:'none',
+
+        '& li': {
+            listStyle: 'none',
         },
     },
-    frontEndIcon:{
-        display:'block',
-        fontSize:100,
+    frontEndIcon: {
+        display: 'block',
+        fontSize: 100,
     }
 
 })
 
-const Skills=()=>{
-    const classes=useStyle();
-    return(
-    <div className={classes.skillsWrapper}>
-        <div className={classes.skillsHeader}>
-            What I Learnt<span>🚀🚀</span>
-        </div>
-    
-    <Row className={classes.mainRow} gutter={{ xs: 1, sm: 16, md: 3, lg: 32 }}justify="space-between">
-      <Col className="gutter-row" span={8} xs={8}>
-        <div>
-            <div className={classes.skillTitle}>
-            <FundProjectionScreenOutlined className={classes.frontEndIcon}/>
-               <p>FRONT-END</p> 
-                <li>React JS</li>
-                <li>Bootstrap</li>     
-                <li>CSS3</li>
-                <li>HTML5</li>                   
+const Skills = () => {
+    const classes = useStyle();
+    return (
+        <div className={classes.skillsWrapper}>
+            <div className={classes.skillsHeader}>
+                What I Learnt<span>🚀🚀</span>
             </div>
-        </div>
-      </Col>
-      <Col className="gutter-row" span={8}>
-        <div >
-        <div className={classes.skillTitle}>
-            <AntDesignOutlined className={classes.frontEndIcon}/>
-               <p>BACK-END</p>
-                <li>Firebase</li>
-                <li>MySQL</li>     
-                <li>MS SQL</li>    
-        </div>
-        </div>
-      </Col>
-      <Col className="gutter-row" span={8}>
-        <div ><div >
-        <div className={classes.skillTitle}>
-            <ToolOutlined className={classes.frontEndIcon}/>
-                <p>TOOLS</p>
-                <li>Github</li>
-        </div>
-        </div></div>
-      </Col>
-    </Row>
+
+            <Row className={classes.mainRow}>
+                <Col span={8} xs={24} lg={8} md={8} >
+                    <div>
+                        <div className={classes.skillTitle}>
+                            <FundProjectionScreenOutlined className={classes.frontEndIcon} />
+                            <p>FRONT-END</p>
+                            <li>React JS</li>
+                            <li>Bootstrap</li>
+                            <li>CSS3</li>
+                            <li>HTML5</li>
+                        </div>
+                    </div>
+                </Col>
+                <Col className="gutter-row" md={8} xs={24} lg={8} span={8}>
+                    <div >
+                        <div className={classes.skillTitle}>
+                            <AntDesignOutlined className={classes.frontEndIcon} />
+                            <p>BACK-END</p>
+                            <li>Firebase</li>
+                            <li>MySQL</li>
+                            <li>MS SQL</li>
+                        </div>
+                    </div>
+                </Col>
+                <Col className="gutter-row" lg={8} xs={24} md={8} span={8}>
+                    <div ><div >
+                        <div className={classes.skillTitle}>
+                            <ToolOutlined className={classes.frontEndIcon} />
+                            <p>TOOLS</p>
+                            <li>Github</li>
+                        </div>
+                    </div></div>
+                </Col>
+            </Row>
         </div>
     )
 }
